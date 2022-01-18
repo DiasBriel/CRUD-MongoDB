6f387e50699b9ffb2653d057ac5dd2fd2c6e1353
Gabriel Dias <g.dias1@hotmail.com>
2022-01-18 19:43:41 -0300

    mongoDB Atlas Connection

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const moongose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
+
+const DB_USER = process.env.USER;
+const DB_PASSWORD = process.env.PASSWORD;
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,4 +17,12 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(3000);
+moongose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@crudcluster.pduiq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000);
+    console.log("MongoDB connected!");
+  })
+  .catch((err) => console.log(err));

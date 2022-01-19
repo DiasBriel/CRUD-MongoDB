47420e0b4ec3ea61509860faf7bcca995962cdd3
Gabriel Dias <g.dias1@hotmail.com>
2022-01-18 21:17:33 -0300

    GET and GET By ID methods

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -7,6 +7,7 @@ router.post("/", async (req, res) => {
 
   if (!name) {
     res.status(422).json({ message: "You must provide a name!" });
+    return;
   }
 
   const person = {
@@ -21,6 +22,34 @@ router.post("/", async (req, res) => {
     res.status(201).json({ message: "Person created successfuly!" });
   } catch (error) {
     res.status(500).json({ error: error }); //this is not the best practice!!! it's just demonstrative
+  }
+});
+
+//List people
+router.get("/", async (req, res) => {
+  try {
+    const people = await Person.find();
+    res.status(200).json(people);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const person = await Person.findOne({ _id: userId });
+    //const person = await Person.findById(userId); --> work too!
+
+    if (!person) {
+      res.status(422).json({ message: "User not found..." });
+      return;
+    }
+
+    res.status(200).json(person);
+  } catch (error) {
+    res.status(500).json({ error: error });
   }
 });
 
